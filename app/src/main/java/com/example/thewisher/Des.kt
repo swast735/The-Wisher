a850d84be6d14aa194b2f7783dc2ad44d81a6d2c
swast735 <133983059+swast735@users.noreply.github.com>
2023-07-03 23:35:26 +0530

    Version1

---
+++ app/src/main/java/com/example/thewisher/Des.kt
@@ -0,0 +1,11 @@
+package com.example.thewisher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Des : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_des)
+    }
+}
--- app/src/main/java/com/example/thewisher/Select_elems.kt
+++ app/src/main/java/com/example/thewisher/Select_elems.kt
@@ -1,11 +1,28 @@
 package com.example.thewisher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Select_elems : AppCompatActivity() {
+    lateinit var name:EditText
+    lateinit var  quote:EditText
+    lateinit var nxtBtn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_elems)
+        name=findViewById(R.id.name)
+        quote=findViewById(R.id.quote)
+        nxtBtn=findViewById(R.id.btnNxt)
+        val strName=name.text.toString()
+        val strQt=quote.text.toString()
+        nxtBtn.setOnClickListener {
+            val i=Intent(this@Select_elems,Des::class.java)
+            i.putExtra("name",strName)
+            i.putExtra("quote",strQt)
+            startActivity(i)
+        }
     }
 }
